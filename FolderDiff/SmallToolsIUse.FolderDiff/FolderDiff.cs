198c91b4674597afe503ed008c685100ab5380a6
Cosmin Vana <Cosmin.Vana@qubiz.com>
2024-08-15 14:46:37 +0300

    Add FolderDiff

---
+++ FolderDiff/SmallToolsIUse.FolderDiff/FolderDiff.cs
@@ -0,0 +1,59 @@
+﻿namespace SmallToolsIUse
+{
+    public class FolderDiff(string sourcePath, string targetPath)
+    {
+        private readonly string sourcePath = sourcePath;
+        private readonly string targetPath = targetPath;
+
+        public FolderDiffResult Compare()
+        {
+            IEnumerable<string> sourceFiles = Directory.EnumerateFiles(sourcePath, "*.*", SearchOption.AllDirectories).Select(f => f.Replace(sourcePath, string.Empty));
+            IEnumerable<string> targetFiles = Directory.EnumerateFiles(targetPath, "*.*", SearchOption.AllDirectories).Select(f => f.Replace(targetPath, string.Empty));
+
+            List<string> onlyInSource = sourceFiles.Except(targetFiles).ToList();
+            List<string> onlyInTarget = targetFiles.Except(sourceFiles).ToList();
+            List<string> inBothButDifferent = sourceFiles.Intersect(targetFiles)
+                .Where(f => new FileInfo($"{sourcePath}\\{f}").Length != new FileInfo($"{targetPath}\\{f}").Length)
+                .ToList();
+            
+            IEnumerable<string> sourceFolders = Directory.EnumerateDirectories(sourcePath, "*", SearchOption.AllDirectories).Select(f => f.Replace(sourcePath, string.Empty));
+            IEnumerable<string> targetFolders = Directory.EnumerateDirectories(targetPath, "*", SearchOption.AllDirectories).Select(f => f.Replace(sourcePath, string.Empty));
+            List<string> foldersOnlyInSource = sourceFolders.Except(targetFolders).ToList();
+            List<string> foldersOnlyInTarget = targetFolders.Except(sourceFolders).ToList();
+
+
+            return new FolderDiffResult(onlyInSource, onlyInTarget, inBothButDifferent, foldersOnlyInSource, foldersOnlyInTarget);
+        }
+    }
+
+    public class FolderDiffResult
+    {
+        public List<string> FilesOnlyInSource { get; }
+        public List<string> FilesOnlyInTarget { get; }
+        public List<string> FilesInBothButDifferent { get; }
+        public List<string> FoldersOnlyInSource { get; }
+        public List<string> FoldersOnlyInTarget { get; }
+
+        public FolderDiffResult(List<string> filesOnlyInSource, 
+            List<string> filesOnlyInTarget, 
+            List<string> filesInBothButDifferent,
+            List<string> foldersOnlyInSource,
+            List<string> foldersOnlyInTarget)
+        {
+            FilesOnlyInSource = filesOnlyInSource;
+            FilesOnlyInTarget = filesOnlyInTarget;
+            FilesInBothButDifferent = filesInBothButDifferent;
+            FoldersOnlyInSource = foldersOnlyInSource;
+            FoldersOnlyInTarget = foldersOnlyInTarget;
+        }
+
+        public bool FoldersAreIdentical()
+        {
+            return FilesOnlyInSource.Count == 0 &&
+                FilesOnlyInTarget.Count == 0 &&
+                FilesInBothButDifferent.Count == 0 &&
+                FoldersOnlyInSource.Count == 0 &&
+                FoldersOnlyInTarget.Count == 0;
+        }
+    }
+}
---
+++ FolderDiff/SmallToolsIUse.FolderDiff/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace SmallToolsIUse;
+
+public class FolderDiffConsole
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Comparing Folders:");
+        Console.WriteLine($"Source: {args[0]}");
+        Console.WriteLine($"Target: {args[1]}");
+        Console.WriteLine();
+
+        var folderDiff = new FolderDiff(args[0], args[1]);
+        var result = folderDiff.Compare();
+
+        if (result.FilesOnlyInSource.Count > 0)
+        {
+            Console.WriteLine($"Files only in {args[0]}");
+            foreach (var item in result.FilesOnlyInSource)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        if(result.FilesOnlyInTarget.Count > 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Files only in {args[1]}");
+            foreach (var item in result.FilesOnlyInTarget)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        if (result.FilesInBothButDifferent.Count > 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Files in both, but different size:");
+            foreach (var item in result.FilesInBothButDifferent)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        if (result.FoldersOnlyInSource.Count > 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Folders only in source:");
+            foreach (var item in result.FoldersOnlyInSource)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        if (result.FoldersOnlyInTarget.Count > 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Folders only in target:");
+            foreach (var item in result.FoldersOnlyInTarget)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        if (result.FoldersAreIdentical())
+        {
+            Console.WriteLine("The two folders are identical");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("Summary");
+            Console.WriteLine($"Files only in source: {result.FilesOnlyInSource.Count()}");
+            Console.WriteLine($"Files only in target: {result.FilesOnlyInTarget.Count()}");
+            Console.WriteLine($"Files in both, but different size: {result.FilesInBothButDifferent.Count()}");
+            Console.WriteLine($"Folders only in source: {result.FoldersOnlyInSource.Count()}");
+            Console.WriteLine($"Folders only in target: {result.FoldersOnlyInTarget.Count()}");
+        }
+    }
+}
