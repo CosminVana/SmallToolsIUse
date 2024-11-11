50fed2affcb3985fea71c32f6b3099a6958b7931
Cosmin Vana <cosminvana@outlook.com>
2024-11-11 11:09:29 +0200

    Add SmallToolsIUse.Linq.Extensions

---
+++ FolderDiff/SmallToolsIUse.Linq.Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmallToolsIUse.Linq.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Checks if the provided IEnumerable instance is null or empty
+        /// </summary>
+        /// <param name="enumerable">IEnumerable instance to check</param>
+        /// <returns>A boolean value indicating if the collection is null or empty</returns>
+        public static bool IsNullOrEmpty(this IEnumerable enumerable) => enumerable == null || !enumerable.GetEnumerator().MoveNext();
+
+        /// <summary>
+        /// Will return all the elements which exist in source IEnumerable, but not in destination IEnumerable, based on the key selector
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <param name="keySelector">The key which should be used to compare the values of the two IEnumerable</param>
+        /// <returns></returns>
+        public static IEnumerable<TCollection> NotIn<TCollection, TKey>(this IEnumerable<TCollection> source,
+            IEnumerable<TCollection> destination, Func<TCollection, TKey> keySelector) where TCollection : class
+        {
+            IEnumerable<TCollection> notIn = from s in source
+                                             join d in destination
+                                                 on keySelector(s) equals keySelector(d) into g
+                                             from element in g.DefaultIfEmpty()
+                                             where element == null
+                                             select s;
+
+            return notIn;
+        }
+
+        /// <summary>
+        /// Will return all the elements which exist in both the source and destination IEnumerable, based on the key selector
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <param name="keySelector">The key which should be used to compare the values of the two IEnumerable</param>
+        public static IEnumerable<TCollection> In<TCollection, TKey>(this IEnumerable<TCollection> source,
+            IEnumerable<TCollection> destination, Func<TCollection, TKey> keySelector) where TCollection : class
+        {
+            IEnumerable<TCollection> inDestination = from s in source
+                                                     join d in destination
+                                                         on keySelector(s) equals keySelector(d)
+                                                     select s;
+
+            return inDestination;
+        }
+
+        /// <summary>
+        /// Will return all the elements which exist in source IEnumerable, but not in destination IEnumerable, based on the key selector
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <param name="sourceKeySelector">The key which should be used for the source IEnumerable</param>
+        /// <param name="destKeySelector">The key which should be used for the destination IEnumerable</param>
+        /// <returns></returns>
+        public static IEnumerable<TSourceCollection> In<TSourceCollection, TDestinationCollection, TKey>(this IEnumerable<TSourceCollection> source,
+            IEnumerable<TDestinationCollection> destination, Func<TSourceCollection, TKey> sourceKeySelector, Func<TDestinationCollection, TKey> destKeySelector) where TSourceCollection : class
+        {
+            IEnumerable<TSourceCollection> In = from s in source
+                                                join d in destination
+                                                    on sourceKeySelector(s) equals destKeySelector(d)
+                                                select s;
+
+            return In;
+        }
+
+        /// <summary>
+        /// Will return all the elements which exist in both the source and destination IEnumerable, based on the key selector for each collection
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <param name="sourceKeySelector">The key which should be used for the source IEnumerable</param>
+        /// <param name="destKeySelector">The key which should be used for the destination IEnumerable</param>
+        /// <returns></returns>
+        public static IEnumerable<TSourceCollection> NotIn<TSourceCollection, TDestinationCollection, TKey>(this IEnumerable<TSourceCollection> source,
+            IEnumerable<TDestinationCollection> destination, Func<TSourceCollection, TKey> sourceKeySelector, Func<TDestinationCollection, TKey> destKeySelector) where TSourceCollection : class
+        {
+            IEnumerable<TSourceCollection> notIn = from s in source
+                                                   join d in destination
+                                                       on sourceKeySelector(s) equals destKeySelector(d) into g
+                                                   from element in g.DefaultIfEmpty()
+                                                   where element == null
+                                                   select s;
+
+            return notIn;
+        }
+    }
+}
---
+++ FolderDiff/SmallToolsIUse.Linq.Extensions/IQueryableExtensions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Reflection;
+
+namespace SmallToolsIUse.Linq.Extensions
+{
+    public static class IQueryableExtensions
+    {
+        private const string Descending = "desc";
+        private const string OrderDescending = "OrderByDescending";
+        private const string OrderAscending = "OrderBy";
+        private const string ThenByDescending = "ThenByDescending";
+        private const string ThenByAscending = "ThenBy";
+
+        public static IQueryable<T> OrderByField<T>(this IQueryable<T> query, string fieldName, string orderDirection)
+        {
+            if (!string.IsNullOrEmpty(fieldName))
+            {
+                string method =
+                    !string.IsNullOrWhiteSpace(orderDirection) && orderDirection.ToLower().Trim().Equals(Descending)
+                        ? OrderDescending
+                        : OrderAscending;
+
+                return ApplyOrder<T>(query, fieldName, method);
+            }
+
+            return query;
+        }
+
+        public static IQueryable<T> ThenByField<T>(this IQueryable<T> query, string fieldName, string orderDirection)
+        {
+            if (!string.IsNullOrEmpty(fieldName))
+            {
+                string method =
+                !string.IsNullOrWhiteSpace(orderDirection) && orderDirection.ToLower().Trim().Equals(Descending)
+                    ? ThenByDescending
+                    : ThenByAscending;
+
+                return ApplyOrder<T>(query, fieldName, method);
+            }
+
+            return query;
+        }
+
+        public static IQueryable<T> SortAndPaginate<T>(this IQueryable<T> source, string sortBy, string sortDirection, int skip, int take)
+        {
+            source = source.OrderByField(sortBy, sortDirection);
+            return source.Paginate(skip, take);
+        }
+
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> source, int skip, int take)
+        {
+            return source.Skip(skip).Take(take);
+        }
+
+        private static IOrderedQueryable<T> ApplyOrder<T>(IQueryable<T> source, string property, string methodName)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(property))
+                {
+                    return source.OrderBy(e => e.GetType().GetProperties().FirstOrDefault());
+                }
+
+                string[] props = property.Split('.');
+                Type type = typeof(T);
+                ParameterExpression arg = Expression.Parameter(type, "x");
+                Expression expr = arg;
+
+                foreach (string prop in props)
+                {
+                    // use reflection (not ComponentModel) to mirror LINQ
+                    PropertyInfo pi = type.GetProperty(prop,
+                        BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+                    if (pi == null)
+                    {
+                        pi = type.GetProperties().First();
+                        expr = Expression.Property(expr, pi);
+                        break;
+                    }
+
+                    expr = Expression.Property(expr, pi);
+                    type = pi.PropertyType;
+                }
+
+                Type delegateType = typeof(Func<,>).MakeGenericType(typeof(T), type);
+                LambdaExpression lambda = Expression.Lambda(delegateType, expr, arg);
+
+                object result = typeof(Queryable).GetMethods().Single(
+                        method => method.Name == methodName
+                                  && method.IsGenericMethodDefinition
+                                  && method.GetGenericArguments().Length == 2
+                                  && method.GetParameters().Length == 2)
+                    .MakeGenericMethod(typeof(T), type)
+                    .Invoke(null, new object[] { source, lambda });
+
+                return (IOrderedQueryable<T>)result;
+            }
+            catch
+            {
+                return (IOrderedQueryable<T>)source;
+            }
+        }
+    }
+}
