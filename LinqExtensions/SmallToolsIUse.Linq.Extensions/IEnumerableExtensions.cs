f1634238aa818235dea161fe09dc8ca944561610
Cosmin Vana <cosminvana@outlook.com>
2024-12-29 09:46:58 +0200

    LinqExtensions - Add ModifiedIn IEnumerable extension

--- LinqExtensions/SmallToolsIUse.Linq.Extensions/IEnumerableExtensions.cs
+++ LinqExtensions/SmallToolsIUse.Linq.Extensions/IEnumerableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmallToolsIUse.Linq.Extensions.Model;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -89,6 +90,50 @@ namespace SmallToolsIUse.Linq.Extensions
                                                    select s;
 
             return notIn;
+        }
+
+        /// <summary>
+        /// Will return all the elements which exist in both the source and destination IEnumerable, based on the key selector for each collection, but which are different based on the comparison predicate
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <param name="keySelector">The key which should be used to compare the values of the two IEnumerable</param>
+        /// <param name="comparisonPredicate">A predicate which decides if the 2 items are different</param>
+        /// <returns></returns>
+        public static IEnumerable<EnumerableItemDiff<TCollection>> ModifiedIn<TCollection, TKey>(this IEnumerable<TCollection> source, IEnumerable<TCollection> destination, Func<TCollection, TKey> keySelector, Func<TCollection, TCollection, bool> comparisonPredicate)
+        {
+            return from s in source
+                   join d in destination
+                       on keySelector(s) equals keySelector(d)
+                   where comparisonPredicate(s, d)
+                   select new EnumerableItemDiff<TCollection>
+                   {
+                       Source = s,
+                       Destination = d
+                   };
+        }
+
+        /// <summary>
+        /// Will return all the elements which exist in both the source and destination IEnumerable, based on the key selector for each collection, but which are different based on the comparison predicate
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="destination"></param>
+        /// <param name="sourceKeySelector">The key which should be used for the source IEnumerable</param>
+        /// <param name="destKeySelector">The key which should be used for the destination IEnumerable</param>
+        /// <returns></returns>
+        /// <param name="comparisonPredicate">A predicate which decides if the 2 items are different</param>
+        /// <returns></returns>
+        public static IEnumerable<EnumerableItemDiff<TSourceCollection, TDestinationCollection>> ModifiedIn<TSourceCollection, TDestinationCollection, TKey>(this IEnumerable<TSourceCollection> source, IEnumerable<TDestinationCollection> destination, Func<TSourceCollection, TKey> sourceKeySelector, Func<TDestinationCollection, TKey> destKeySelector, Func<TSourceCollection, TDestinationCollection, bool> comparisonPredicate)
+        {
+            return from s in source
+                   join d in destination
+                       on sourceKeySelector(s) equals destKeySelector(d)
+                   where comparisonPredicate(s, d)
+                   select new EnumerableItemDiff<TSourceCollection, TDestinationCollection>
+                   {
+                       Source = s,
+                       Destination = d
+                   };
         }
     }
 }
---
+++ LinqExtensions/SmallToolsIUse.Linq.Extensions/Model/EnumerableItemDiff.cs
@@ -0,0 +1,28 @@
+﻿namespace SmallToolsIUse.Linq.Extensions.Model
+{
+    public class EnumerableItemDiff<T>
+    {
+        /// <summary>
+        /// Element instance in source collection
+        /// </summary>
+        public T Source { get; set; }
+
+        /// <summary>
+        /// Element instance in destination collection
+        /// </summary>
+        public T Destination { get; set; }
+    }
+
+    public class EnumerableItemDiff<TSource, TDestination>
+    {
+        /// <summary>
+        /// Element instance in source collection
+        /// </summary>
+        public TSource Source { get; set; }
+
+        /// <summary>
+        /// Element instance in destination collection
+        /// </summary>
+        public TDestination Destination { get; set; }
+    }
+}
